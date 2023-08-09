a9d21aef10300361df5393dc70c159615beb878c
Jordy <jordybeer@duck.com>
2023-08-09 22:13:41 +0200

    Update index.jsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+
+// Fixed script tag
+import React, { useEffect } from "react";
 
 const IndexPage = () => {
+  useEffect(() => {
+    setTimeout(function() {
+      document.getElementById("body").style.display = "";        
+    }, 200);
+
+  }, []);
+
   return (
     <>
-
+      <div id="body" style={{ display: "none" }}>
+        {/* Your page content goes here */}
+      </div>
     </>
   );
 };
