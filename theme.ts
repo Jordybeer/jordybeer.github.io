2d2ba6f33bbf4ce229dc4b3cd0756bb1245115c7
Jordy <jordybeer@duck.com>
2023-08-22 19:03:49 +0200

    dark mode by default

--- theme.ts
+++ theme.ts
@@ -1,9 +1,12 @@
 import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
+    initialColorMode: 'dark',
+  useSystemColorMode: false,
   styles: {
     global: {
       "*": {
+        dark
         textShadow: "1px 1px 2px rgba(0,0,0,0.5)",
       },
     },
