9b9ff3e21b8e4aeeba5501d8443bce507d0cdfc2
jordybeer <jordybeer@duck.com>
2023-12-03 15:45:41 +0100

    merge

--- components/Card.tsx
+++ components/Card.tsx
@@ -7,16 +7,13 @@ type CardProps = {
 
 const Card = ({ data, setSelectedCard }: CardProps) => {
   const handleClick = () => {
-<<<<<<< HEAD
     console.log("Card clicked:", data); // Log de data bij klikken
     setSelectedCard(data);
-=======
     if (setSelectedCard && typeof setSelectedCard === 'function') {
       setSelectedCard(data);
     } else {
       console.error('setSelectedCard is not a function');
     }
->>>>>>> 7b4128ca0cf235e6658c4bc98bfdc6e1df467064
   };
 
   const handleClick = () => {
@@ -31,11 +28,8 @@ const Card = ({ data, setSelectedCard }: CardProps) => {
     <motion.div
       className="card"
       p={20}
-<<<<<<< HEAD
       onClick={handleClick} // Gebruik de handleClick functie hier
-=======
       onClick={handleClick}
->>>>>>> 7b4128ca0cf235e6658c4bc98bfdc6e1df467064
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
     >
